3b89f864e4dea5e0d210ce0736c3821c0bd6544a
Ashar Farooq <ashar744@gmail.com>
2021-08-06 02:10:55 -0400

    styling login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {!loggedIn ? 
+      <div className="login"> 
+      <br/>
+      <br/>
+      <br/>
+        <div className="form-group">
+        <label className="form-label basic" htmlFor="name">Enter Name:</label>
+        <input id="name" className="form-control" type="text" placeholder="Enter Name Here" />
+        </div>
+        <div className="form-group">
+        <label className="form-label basic" htmlFor="room">Enter Room:</label>
+        <input id="room" className="form-control " type="text" placeholder="Enter Room Here" />
+        </div>
+        <button className="btn btn-primary">Enter Chat</button>
+      </div>
+      : 
+      <h1>Logged In</h1>
+    }
     </div>
   );
 }
