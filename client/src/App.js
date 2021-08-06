6a561d65c30a7f8e024b3ecdd031254cc535a8d4
Ashar Farooq <ashar744@gmail.com>
2021-08-06 02:27:42 -0400

    adding client socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
+let socket;
 function App() {
 
   const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+
+
+  const CONNECTION = "http://localhost:3002/";
+
+useEffect(()=> {
+  socket = io(CONNECTION);
+}, [CONNECTION]);
+
+  const connectRoom = () => {
+      socket.emit('join_room', room);
+  };
 
   return (
+
     <div className="App">
     {!loggedIn ? 
       <div className="login"> 
@@ -14,13 +30,13 @@ function App() {
       <br/>
         <div className="form-group">
         <label className="form-label basic" htmlFor="name">Enter Name:</label>
-        <input id="name" className="form-control" type="text" placeholder="Enter Name Here" />
+        <input onChange={(event) => setUserName(event.target.value)} id="name" className="form-control" type="text" placeholder="Enter Name Here" />
         </div>
         <div className="form-group">
         <label className="form-label basic" htmlFor="room">Enter Room:</label>
-        <input id="room" className="form-control " type="text" placeholder="Enter Room Here" />
+        <input onChange={(event) => setRoom(event.target.value)} id="room" className="form-control " type="text" placeholder="Enter Room Here" />
         </div>
-        <button className="btn btn-primary">Enter Chat</button>
+        <button onClick={connectRoom} className="btn btn-primary">Enter Chat</button>
       </div>
       : 
       <h1>Logged In</h1>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const socket = require("socket.io");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-
-app.listen("3002", (req,res) => {
+const server = app.listen("3002", (req,res) => {
     console.log("Server running on PORT.");
 });
 
+io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('join_room', (data) => {
+        socket.join(data);
+        console.log("User joined Room: " + data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log("User Disconnected.")
+    });
+});
